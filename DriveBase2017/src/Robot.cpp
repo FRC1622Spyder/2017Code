d99c378eb6832cc48ad21059799e8e3149be8c5f
miner3203 <zack@siliconwhiteboard.com>
2017-01-25 19:02:09 -0800

    Added comments to drive code

--- DriveBase2017/src/Robot.cpp
+++ DriveBase2017/src/Robot.cpp
@@ -12,19 +12,18 @@
 class Robot:public IterativeRobot {
 
 private:
-	int replaceThisInt;
 
-	Joystick *driveStick = new Joystick(0);
+	Joystick *driveStick = new Joystick(0); //Declare joystick and motors
 	CANTalon *leftBackMotor = new CANTalon(9);
 	CANTalon *leftFrontMotor = new CANTalon(6);
 	CANTalon *rightBackMotor = new CANTalon(7);
 	CANTalon *rightFrontMotor = new CANTalon(8);
 
-	double speedValueLeft = 0.0;
+	double speedValueLeft = 0.0; //Declare current speed
 	double speedValueRight = 0.0;
 
-	double leftPast = 0.0;
-	double rightPast = 0.0;
+	//double leftPast = 0.0; //Ramp code - removed because it's currently unused
+	//double rightPast = 0.0;
 
 	/*double accelerateLeft(double maxSpeed) {
 		//std::cout << "LeftStick: " << maxSpeed << std::endl;
@@ -122,17 +121,17 @@ private:
 		}
 	}*/
 
-	void driveLeft(double speed){
+	void driveLeft(double speed){ //Drives left motors
 		leftFrontMotor->Set(speed);
 		leftBackMotor->Set(speed);
 	}
 
-	void driveRight(double speed){
+	void driveRight(double speed){ //Drives right motors
 		rightFrontMotor->Set(speed);
 		rightBackMotor->Set(speed);
 	}
 
-	/*void driveRobot(){
+	/*void driveRobot(){ //Ramp code
 		double leftStick = driveStick->GetRawAxis(1);
 		double rightStick = driveStick->GetRawAxis(3);
 		driveLeft(accelerateLeft(leftStick));
@@ -140,7 +139,7 @@ private:
 	}*/
 
 	void RobotInit() {
-		leftBackMotor->SetInverted(true);
+		leftBackMotor->SetInverted(true); //Inverts left motors
 		leftFrontMotor->SetInverted(true);
 	}
 
@@ -154,37 +153,37 @@ private:
 	}
 
 	void TeleopPeriodic() {
-		//driveRobot();
-		double leftSpeed = driveStick->GetRawAxis(1);
-		if(leftSpeed > 0.2 && leftSpeed > 0.8){
+		//driveRobot(); //Ramp code
+		double leftSpeed = driveStick->GetRawAxis(1); //Get left joystick
+		if(leftSpeed > 0.2 && leftSpeed > 0.8){ //If speed is positive, outside dead zone, and above 80% (set to max speed)
 			leftSpeed = 0.8;
 		}
-		else if(leftSpeed >= 0.2 && leftSpeed <= 0.5){
+		else if(leftSpeed >= 0.2 && leftSpeed <= 0.5){ //If speed is positive, outside dead zone, and below 50% (set to half speed)
 			leftSpeed = leftSpeed / 2;
 		}
-		else if(leftSpeed < -0.2 && leftSpeed < -0.8){
+		else if(leftSpeed < -0.2 && leftSpeed < -0.8){ //If speed is negative, outside dead zone, and below -80% (set to max speed)
 			leftSpeed = -0.8;
 		}
-		else if(leftSpeed <= -0.2 && leftSpeed >= -0.5){
+		else if(leftSpeed <= -0.2 && leftSpeed >= -0.5){ //If speed is negative, outside dead zone, and above -50% (set to half speed)
 			leftSpeed = leftSpeed / 2;
 		}
-		else if(leftSpeed > -0.2 && leftSpeed < 0.2){
+		else if(leftSpeed > -0.2 && leftSpeed < 0.2){ //If speed is in dead zone (set to 0)
 			leftSpeed = 0;
 		}
-		double rightSpeed = driveStick->GetRawAxis(3);
-		if(rightSpeed > 0.2 && rightSpeed > 0.8){
+		double rightSpeed = driveStick->GetRawAxis(3); //Get right joystick
+		if(rightSpeed > 0.2 && rightSpeed > 0.8){ //If speed is positive, outside dead zone, and above 80% (set to max speed)
 			rightSpeed = 0.8;
 		}
-		else if(rightSpeed >= 0.2 && rightSpeed <= 0.5){
+		else if(rightSpeed >= 0.2 && rightSpeed <= 0.5){ //If speed is positive, outside dead zone, and below 50% (set to half speed)
 			rightSpeed = rightSpeed / 2;
 		}
-		else if(rightSpeed < -0.2 && rightSpeed < -0.8){
+		else if(rightSpeed < -0.2 && rightSpeed < -0.8){ //If speed is negative, outside dead zone, and below -80% (set to max speed)
 			rightSpeed = -0.8;
 		}
-		else if(rightSpeed <= -0.2 && rightSpeed >= -0.5){
+		else if(rightSpeed <= -0.2 && rightSpeed >= -0.5){ //If speed is negative, outside dead zone, and above -50% (set to half speed)
 			rightSpeed = rightSpeed / 2;
 		}
-		else if(rightSpeed > -0.2 && rightSpeed < 0.2){
+		else if(rightSpeed > -0.2 && rightSpeed < 0.2){ //If speed is in dead zone (set to 0)
 			rightSpeed = 0;
 		}
 		driveLeft(leftSpeed);
